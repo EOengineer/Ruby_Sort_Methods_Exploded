ff2e75f4a13ea945ffbd7097480cded6482e111a
EOengineer <EOengineer@gmail.com>
2014-05-05 16:34:29 -0400

    methos created and commited

---
+++ sort.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+def exploded_sort(array)
+  counter = 0
+  sorted_array = Array.new
+  size = array.length
+
+  array.each do |value|
+
+  end
+
+
+puts sorted_array
+end
+
+problem = [5,6,1,7,9,2,4,3,8]
+exploded_sort(problem)
