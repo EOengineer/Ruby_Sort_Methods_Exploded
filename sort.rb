6c5c803398087e123d3772e9c858fe85b89adaf1
EOengineer <EOengineer@gmail.com>
2014-05-07 19:22:12 -0400

    killed and rebuilding

--- sort.rb
+++ sort.rb
@@ -1,16 +1,10 @@
 require 'pry'
 
-def exploded_sort(array)
-  counter = 0
-  sorted_array = Array.new
-  size = array.length
-
-  array.each do |value|
-
+class Array
+  def exploded_sort
+    sorted_array = Array.new
+    index = 0
   end
-
-
-puts sorted_array
 end
 
 problem = [5,6,1,7,9,2,4,3,8]
